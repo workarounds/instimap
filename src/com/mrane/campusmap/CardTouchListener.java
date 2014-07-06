a3dd2503377c9f90be2293906dbfc866fa47e450
Manideep Polireddi <manideepiitb@gmail.com>
2014-07-07 04:43:32 +0530

    show card animation implemented

--- src/com/mrane/campusmap/CardTouchListener.java
+++ src/com/mrane/campusmap/CardTouchListener.java
@@ -3,10 +3,20 @@ package com.mrane.campusmap;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CardTouchListener implements OnTouchListener {
 
+	private RelativeLayout bottomLayout;
+	private RelativeLayout placeCard;
+	private TextView placeNameTextView;
+	private float density;
+	private float displayHeight;
+	private float displayWidth;
 	@Override
 	public boolean onTouch(View arg0, MotionEvent arg1) {
 		Toast.makeText(MainActivity.getmMainActivity(), "Card touched",
@@ -15,6 +25,24 @@ public class CardTouchListener implements OnTouchListener {
 	}
 
 	public class AnimationBuilder{
-		
+		public Animation showCardAnimation(){
+			int xType = Animation.ABSOLUTE;
+			float xValueFrom = bottomLayout.getLeft();
+			float xValueTo = bottomLayout.getLeft();
+			int yType = Animation.ABSOLUTE;
+			float yValueFrom = bottomLayout.getTop();
+			float yValueTo = displayHeight - (bottomLayout.getTop() - placeCard.getTop()) -  placeNameTextView.getPaddingTop()*2 - 80*density;
+			TranslateAnimation anim = new TranslateAnimation(xType, xValueFrom, xType, xValueTo, yType, yValueFrom, yType, yValueTo);
+			return anim;
+		}
+		public Animation expandCardAnimation(){
+			return null;
+		}
+		public Animation collapseCardAnimation(){
+			return null;
+		}
+		public Animation dismissCardAnimation(){
+			return null;
+		}
 	}
 }

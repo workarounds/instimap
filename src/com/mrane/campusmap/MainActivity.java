08ac94c705956a4f2f7b4e8ec3f4d9364e57e0f8
Madhu kiran <p.madhu.kiran96@gmail.com>
2014-07-06 15:38:51 +0530

    added new launcher icon

--- src/com/mrane/campusmap/MainActivity.java
+++ src/com/mrane/campusmap/MainActivity.java
@@ -4,12 +4,10 @@ import java.util.HashMap;
 import java.util.Set;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;

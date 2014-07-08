e0569ef67c77304de18734073c7127665adbba37
Madhu kiran <p.madhu.kiran96@gmail.com>
2014-07-08 18:40:28 +0530

    sorting array

--- src/com/mrane/campusmap/ListFragment.java
+++ src/com/mrane/campusmap/ListFragment.java
@@ -11,28 +11,27 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ListFragment extends Fragment {
-	
+
 	MapActivity mainActivity;
 	ArrayAdapter<String> adapter;
 	HashMap<String, Marker> data;
 	View rootView;
 	ListView list;
-	
+
 	public ListFragment() {
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		mainActivity = MapActivity.getMainActivity();
 		adapter = mainActivity.getAdapter();
-		rootView = inflater.inflate(R.layout.list_fragment, container,
-				false);
+		rootView = inflater.inflate(R.layout.list_fragment, container, false);
 		list = (ListView) rootView.findViewById(R.id.suggestion_list);
 		list.setAdapter(adapter);
 		list.setOnItemClickListener(mainActivity);
 		list.setOnTouchListener(mainActivity);
-		
+		list.setFastScrollEnabled(true);
 		return rootView;
 	}
 

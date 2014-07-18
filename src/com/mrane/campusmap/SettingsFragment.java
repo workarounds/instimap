b4831abd726fe299a49643def0b93c48886bfe5b
madhu kiran <pmadhukiran96@gmail.com>
2014-07-18 19:01:36 +0530

    not muting system, just muting app now

--- src/com/mrane/campusmap/SettingsFragment.java
+++ src/com/mrane/campusmap/SettingsFragment.java
@@ -10,11 +10,13 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.RelativeLayout;
 
 public class SettingsFragment extends Fragment implements OnClickListener {
 	MapActivity mainActivity;
 	View rootView;
 	CheckBox checkBox;
+	RelativeLayout muteRow;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,23 +25,39 @@ public class SettingsFragment extends Fragment implements OnClickListener {
 		rootView = inflater.inflate(R.layout.settings_fragment, container,
 				false);
 		checkBox = (CheckBox) rootView.findViewById(R.id.mute_checkbox);
+		muteRow = (RelativeLayout) rootView.findViewById(R.id.mute_row);
+		setCorrectCheckBoxState();
+		checkBox.setOnClickListener(this);
+		muteRow.setOnClickListener(this);
+		return rootView;
+	}
+
+	private void setCorrectCheckBoxState() {
 		if (mainActivity.muted) {
 			checkBox.setChecked(true);
 		} else {
 			checkBox.setChecked(false);
 		}
-		checkBox.setOnClickListener(this);
-		return rootView;
+
 	}
 
 	@Override
 	public void onClick(View v) {
-		Log.d("test123","on click called");
+		Log.d("test123", "on click called");
+		if (v instanceof RelativeLayout) {
+			toggleCheckBoxState();
+		}
 		Editor editor = mainActivity.sharedpreferences.edit();
 		editor.putBoolean("mute", checkBox.isChecked());
-		Log.d("test123","on click called with bool : " + checkBox.isChecked());
+		Log.d("test123", "on click called with bool : " + checkBox.isChecked());
 		mainActivity.muted = checkBox.isChecked();
-		// mainActivity.audiomanager.setStreamMute(AudioManager.STREAM_MUSIC, mainActivity.muted);
+		// mainActivity.audiomanager.setStreamMute(AudioManager.STREAM_MUSIC,
+		// mainActivity.muted);
 		editor.commit();
+	}
+
+	private void toggleCheckBoxState() {
+		checkBox.setChecked(!checkBox.isChecked());
+		
 	}
 }

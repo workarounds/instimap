8c2918fb998acecd587b9201d20c729578fdbf8f
Manideep Polireddi <manideepiitb@gmail.com>
2014-07-20 23:34:33 +0530

    bug fixing

--- src/com/mrane/data/Locations.java
+++ src/com/mrane/data/Locations.java
@@ -1679,7 +1679,7 @@ public class Locations {
 		data.get("Hostel 13").showDefault = true;
 		data.get("Hostel 15").showDefault = true;
 		data.get("Main Gate no. 2").showDefault = true;
-		data.get("Market Gate, Y point Gate").showDefault = true;
+		data.get("Market Gate, Y point Gate no. 3").showDefault = true;
 		data.get("Canara Bank").showDefault = true;
 		data.get("Convocation Hall").showDefault = true;
 	}
